c4539b8b1de25695ca04b3c6080e6df794286fad
Piero <pacoronado01@gmail.com>
2025-07-03 13:11:14 -0400

    Add search logic

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,10 +1,31 @@
-export default function Search() {
+import { CiSearch } from "react-icons/ci";
+
+interface SearchProps {
+  submit: (playerName: string) => void;
+}
+
+export default function Search({ submit }: SearchProps) {
+  function handleSubmit(formData: FormData) {
+    const query = formData.get("player-name");
+    if (query) {
+      submit(query.toString());
+    }
+  }
+
   return (
-    <form action="">
-      <label htmlFor="player-name">
-        Player:
-        <input type="text" name="player-name" id="player-name" required />
-      </label>
+    <form className="text-center" action={handleSubmit}>
+      <input
+        className="bg-gray-50 px-2 rounded h-[40px] w-[200px] text-black"
+        type="text"
+        name="player-name"
+        id="player-name"
+        min={3}
+        maxLength={30}
+        required
+      />
+      <button type="submit">
+        <CiSearch />
+      </button>
     </form>
   );
 }
