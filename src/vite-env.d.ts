1f3e40455001ee56d5397b2029ec3068c93989fd
Piero <pacoronado01@gmail.com>
2025-07-03 13:11:25 -0400

    install react-icons

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,35 @@
 /// <reference types="vite/client" />
+interface Player {
+  player: {
+    id: string;
+    name: string;
+    firstname: string;
+    lastname: string;
+    age: number;
+    birth: {
+      date: string;
+      place: string;
+      country: string;
+    };
+    nationality: string;
+    height: string;
+    weight: string;
+    number: number;
+    position: string;
+    photo: string;
+  };
+}
+
+interface PlayerResponse {
+  get: string;
+  parameters: {
+    player: string;
+  };
+  errors: string[];
+  results: string;
+  paging: {
+    current: number;
+    total: number;
+  };
+  response: Player[];
+}
