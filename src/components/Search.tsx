58fa6404477ccb1e4afbdfb618ffbd68f07cb33f
Piero <pacoronado01@gmail.com>
2025-07-03 12:24:37 -0400

    first commit

---
+++ src/components/Search.tsx
@@ -0,0 +1,10 @@
+export default function Search() {
+  return (
+    <form action="">
+      <label htmlFor="player-name">
+        Player:
+        <input type="text" name="player-name" id="player-name" required />
+      </label>
+    </form>
+  );
+}
