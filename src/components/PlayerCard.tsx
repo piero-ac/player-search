89d65d305b3c84f7ee6005f461eb4f4b4db1cfca
Piero <pacoronado01@gmail.com>
2025-07-04 20:12:04 -0400

    Add default picture, make card trigger player statistics when clicked

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -1,11 +1,19 @@
-export default function PlayerCard({ player }: { player: Player }) {
+import BlackProfilePic from "../assets/blank-profile-photo-1.jpg";
+
+export default function PlayerCard({
+  player,
+  selectPlayer,
+}: {
+  player: PlayerProfile;
+  selectPlayer: () => void;
+}) {
   const { player: p } = player;
   return (
-    <div className="h-[200px] text-center self-center">
+    <div onClick={selectPlayer} className="h-[200px] text-center self-center">
       <div className="w-full ">
         <img
           className="aspect-square object-cover rounded-full mx-auto"
-          src={p.photo}
+          src={p.photo ? p.photo : BlackProfilePic}
           alt={`Photo of ${p.name}`}
         />
       </div>
