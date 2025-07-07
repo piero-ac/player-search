20cc1ee677733366b9f08df4bec34c07a2e741eb
Piero <pacoronado01@gmail.com>
2025-07-07 14:40:40 -0400

    Install and set up tanstack router

---
+++ src/routes/__root.tsx
@@ -0,0 +1,17 @@
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+export const Route = createRootRoute({
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
+    <>
+      <Outlet />
+      <ReactQueryDevtools initialIsOpen={false} />
+      <TanStackRouterDevtools />
+    </>
+  );
+}
